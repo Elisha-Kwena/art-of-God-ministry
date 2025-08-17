08666cc13b01a22284c6826f649a035b73e89531
ElishaKwena <elishakwena@gmail.com>
2025-08-17 14:09:33 +0300

    updated loader

--- frontend/src/config/AppRouter.tsx
+++ frontend/src/config/AppRouter.tsx
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { ROUTES } from "../config/routes";
 import { lazy, Suspense } from "react";
 import Layout from "../components/ui/Layout";
+import Loader from "../components/ui/Loader";
 
 const HomePage = lazy(() => import("../pages/home/HomePage"));
 const NotFoundPage = lazy(() => import('../pages/notFound/NotFound'));
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div className="text-center p-8">Loading page...</div>}>
+    <Suspense fallback={<Loader/>}>
       <Routes>
         {/* Wrap routes in Layout */}
         <Route element={<Layout />}>
