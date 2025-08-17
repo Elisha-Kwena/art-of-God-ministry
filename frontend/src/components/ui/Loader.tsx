2f3e3f6e8ce53bf1aaee96f13be465d46db44d9e
ElishaKwena <elishakwena@gmail.com>
2025-08-17 12:23:49 +0300

    loader added

--- frontend/src/components/ui/Loader.tsx
+++ frontend/src/components/ui/Loader.tsx
@@ -1,8 +1,67 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+import bible from '../../assets/images/bible.png'
+const Loader:React.FC = () => {
+  return (
+    <>
+    <div className="w-full h-screen flex flex-col justify-center items-center welcome">
+        <div className="flex items-center justify-center">
+            <img src={bible} alt="" />
+        </div>
+        <StyledWrapper>
+          <div className="spinner">
+            <span />
+            <span />
+            <span />
+          </div>
+        </StyledWrapper>
+    </div>
+    </>
 
-const Loader:React.FC = () =>{
-    return (
-        <h1>Loading...</h1>
-    )
+  );
 }
-export default Loader
+
+const StyledWrapper = styled.div`
+  .spinner {
+   --clr: rgb(0, 113, 128);
+   --gap: 6px;
+     /* gap between each circle */
+   width: 100px;
+   height: 100px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   gap: var(--gap);
+  }
+
+  .spinner span {
+   width: 20px;
+   height: 20px;
+   border-radius: 100%;
+   background-color: var(--clr);
+   opacity: 0;
+  }
+
+  .spinner span:nth-child(1) {
+   animation: fade 1s ease-in-out infinite;
+  }
+
+  .spinner span:nth-child(2) {
+   animation: fade 1s ease-in-out 0.33s infinite;
+  }
+
+  .spinner span:nth-child(3) {
+   animation: fade 1s ease-in-out 0.66s infinite;
+  }
+
+  @keyframes fade {
+   0%, 100% {
+    opacity: 1;
+   }
+
+   60% {
+    opacity: 0;
+   }
+  }`;
+
+export default Loader;
