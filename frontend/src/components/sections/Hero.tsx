44f9c856b499165a1c7058b4e91a695ca71c8836
ElishaKwena <elishakwena@gmail.com>
2025-08-17 16:35:28 +0300

    fixed hero aos

--- frontend/src/components/sections/Hero.tsx
+++ frontend/src/components/sections/Hero.tsx
@@ -13,9 +13,9 @@ const Hero:React.FC = () =>{
             </div>
             <div className="absolute w-full h-full top-0 left-0 backdrop-blur-sm"></div>
             <div className="relative z-10 w-full h-full flex flex-col items-center justify-center">
-                <h1 className="text-6xl font-extrabold font-['impact'] text-white" data-aos="fade-up">Experience God's Presence at The Ark of God Ministry</h1>
-                <p className=" font-extrabold text-2xl text-black" >Join us as we worship, grow, and serve together in Christ’s love.</p>
-                <div className="flex items-center justify-between gap-4 mt-10">
+                <h1 className="text-6xl font-extrabold font-['impact'] text-white" data-aos="fade-up" data-aos-delay="400">Experience God's Presence at The Ark of God Ministry</h1>
+                <p className=" font-extrabold text-2xl text-black" data-aos="fade-up" data-aos-delay="800">Join us as we worship, grow, and serve together in Christ’s love.</p>
+                <div className="flex items-center justify-between gap-4 mt-10" data-aos="fade-up" data-aos-delay="1200">
                     <button className="rounded-md p-4 flex items-center font-extrabold bg-yellow-500 text-white text-xl px-6 hover:bg-gold">Daily Devotion</button>
                     <Link to="" className="flex items-center justify-center gap-4 rounded-md p-4 bg-red">
                     <FontAwesomeIcon icon={faPlay} className="text-2xl text-white"/>
